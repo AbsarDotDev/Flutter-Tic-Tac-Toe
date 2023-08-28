88106403b275698f183fd7158cf64a328dc97184
Absar Ali <absardotdev@Absars-MacBook-Pro.local>
2023-08-28 05:02:20 +0500

    Custom Text Widget Added

---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final List<Shadow> shadows;
+  final double fontSize;
+  const CustomText(
+      {super.key,
+      required this.text,
+      required this.shadows,
+      required this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
