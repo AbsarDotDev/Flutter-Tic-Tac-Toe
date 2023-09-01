3799d3b70ee011938ad53897419084a63ca3e171
FaazLaeeq <faazlaeeq@gmail.com>
2023-09-02 04:42:10 +0500

    index.js work done

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+
+const express = require('express');
+const http = require('http');
+const mongoose= require('mongoose');
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+var server =http.createServer(app);
+
+var io= require('socket.io')(server);//Important install socket,io@2.3.0 version
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    });
+
+app.use(express.json());
+
+const DB=process.env.DB
+mongoose.connect(DB).then(() =>console.log("DB connection successful!")).catch(err => console.log(err));
+
+server.listen(port, "0.0.0.0",() => {
+  console.log(`Server running on port ${port}`);
+});
+
